2168a48c7c25a7dca5fcb742f5829b426ad64bb8
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2026-03-25 11:36:09 +0500

    GTM Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/ui/navbar";
 import Footer from "@/components/ui/footer";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Xntric",
@@ -17,14 +18,40 @@ export default function RootLayout({
     <html lang="en">
       <meta name="robots" content="index, follow"></meta>
 
+      {/* ✅ Google Tag Manager */}
+      <Script
+        id="gtm-head"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NMNKB697');
+            `,
+        }}
+      />
+
       {/* AHREF Script */}
 
       <script src="https://analytics.ahrefs.com/analytics.js" data-key="B5shWJuCtNuL08s0XSID6A"
         async>
       </script>
 
-
       <body className={`bg-black overflow-x-hidden`}>
+        {/* Google Tag Manager */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NMNKB697"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Navbar />
         {children}
         <Navbar position="bottom" />

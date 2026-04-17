c1b8f42a74970a614c4fe7c3b61fb17b877e32cd
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-04-17 14:33:22 +0500

    Generate robots  false

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: 'https://xntric.ca',
-    generateRobotsTxt: true,
+    generateRobotsTxt: false,
     changefreq: 'daily',
     priority: 0.7,
     sitemapSize: 5000,
